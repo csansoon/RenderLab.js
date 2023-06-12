3f065c0f5fc590979d3db887e751bf96a84e4f17
Carlos Sansón <57395395+csansoon@users.noreply.github.com>
2023-06-12 23:36:13 +0200

    New RenderElement: circle

---
+++ components/RenderElements/Circle.js
@@ -0,0 +1,38 @@
+import RenderElement from "../RenderElement.js";
+
+class Circle extends RenderElement {
+
+	constructor(position, radius, color) {
+		super(position);
+		this.radius = radius;
+		this.color = color;
+	}
+
+	getPosition() {
+		return this.position;
+	}
+
+	getBoundingBox() {
+		return {
+			left: this.position.x - this.radius,
+			top: this.position.y - this.radius,
+			right: this.position.x + this.radius,
+			bottom: this.position.y + this.radius,
+		};
+	}
+
+	render(ctx, offset, scale) {
+		const x = (this.position.x + offset.x) * scale.x;
+		const y = (this.position.y + offset.y) * scale.y;
+		const radius = this.radius * scale.x;
+
+		ctx.fillStyle = this.color;
+		ctx.beginPath();
+		ctx.arc(x, y, radius, 0, 2 * Math.PI);
+		ctx.fill();
+
+		console.log("rendered circle");
+	}
+}
+
+export default Circle;
