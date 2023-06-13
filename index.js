fd90645741516c934e97c511e8e09dfc33ec693d
Carlos Sansón <57395395+csansoon@users.noreply.github.com>
2023-06-13 22:20:10 +0200

    Simplified index.js file

--- index.js
+++ index.js
@@ -2,62 +2,43 @@ import World from './components/World.js';
 import Box from './components/RenderElements/Box.js';
 import Circle from './components/RenderElements/Circle.js';
 
+
+/* ---------------------------- Create the world ---------------------------- */
+
 const world = new World();
-const viewport = world.createViewport({ element: "#canvas" });
-const viewport2 = world.createViewport({
-	element: "#canvas2", options: {
-		moveOnDrag: false,
-		zoomOnScroll: false,
-} });
+
+
+/* -------------------- Populate the world with elements -------------------- */
+
+world.add(new Box({ x: 0, y: 0 }, { x: 100, y: 100 }, 'red'));
+world.add(new Box({ x: 100, y: 100 }, { x: 100, y: 100 }, 'green'));
+world.add(new Box({ x: 800, y: 200 }, { x: 100, y: 100 }, 'blue'));
 
 const circle_element = new Circle({ x: 100, y: 100 }, 50, 'yellow');
-
-function init() {
-	const red_box = new Box({ x: 0, y: 0 }, { x: 100, y: 100 }, 'red');
-	const green_box = new Box({ x: 100, y: 100 }, { x: 100, y: 100 }, 'green');
-	const blue_box = new Box({ x: 800, y: 200 }, { x: 100, y: 100 }, 'blue');
-
-	world.add(red_box);
-	world.add(green_box);
-	world.add(blue_box);
-
-	world.add(circle_element);
-
-	viewport.start(60);
-	viewport2.start(60);
-
-	setInterval(update, 1000 / 60);
-}
+world.add(circle_element);
 
 
-/**
- * Linear interpolation: it returns a value between a and b based on t
- * @param {number} a Start value
- * @param {number} b End value
- * @param {number} t Interpolation value (0-1) It represents the percentage of the interpolation
- */
-function lerp(a, b, t) {
-	return a + (b - a) * t;
-}
+/* ------------------ Create virtual cameras for the world ------------------ */
+
+const viewport = world.createViewport({ element: "#canvas" });
+const viewport2 = world.createViewport({
+	element: "#canvas2",
+	options: {
+		moveOnDrag: false,
+		zoomOnScroll: false,
+	}
+});
+
+viewport.start(60);
+viewport2.start(60);
 
 
-const mousePosition = { x: 0, y: 0 };
-function updateMousePosition(e) {
-	mousePosition.x = e.clientX;
-	mousePosition.y = e.clientY;
-}
-document.addEventListener("mousemove", updateMousePosition);
+/* ------------------------ Add behavior to the world ----------------------- */
 
 function update() {
-	const startPosition = circle_element.getPosition();
-	const objectivePosition = viewport.getWorldPosition(mousePosition);
-
-	const delta = {
-		x: lerp(startPosition.x, objectivePosition.x, 0.25),
-		y: lerp(startPosition.y, objectivePosition.y, 0.1),
-	};
-
-	circle_element.setPosition(delta);
+	// Move the circle towards the mouse
+	const mousePositionInTheWorld = viewport.getWorldPosition(mousePosition);
+	circle_element.lerpTo(mousePositionInTheWorld, 0.1);
 
 	// Update the viewport2 position to follow the circle
 	viewport2.setPosition({
@@ -66,4 +47,13 @@ function update() {
 	});
 }
 
-document.addEventListener("DOMContentLoaded", init, false);
+// Run the update function 60 times per second
+setInterval(update, 1000 / 60);
+
+// Keep track of the mouse position
+const mousePosition = { x: 0, y: 0 };
+function updateMousePosition(e) {
+	mousePosition.x = e.clientX;
+	mousePosition.y = e.clientY;
+}
+document.addEventListener("mousemove", updateMousePosition);
