26273481a77f8de094f05316957ee060a0fa673f
Carlos Sansón <57395395+csansoon@users.noreply.github.com>
2023-06-15 22:22:44 +0200

    Simplified Cellular Automata example

--- examples/cellular-automata/index.js
+++ examples/cellular-automata/index.js
@@ -6,16 +6,32 @@ const world = new World();
 const viewport = world.createViewport({ element: "#canvas" });
 viewport.start(60);
 
-// when document is ready
-setTimeout(() => {
-	const automata = new CellularAutomata({
-		world,
-		width: Math.floor(viewport.size.x / 8),
-		height: Math.floor(viewport.size.y / 8),
-		cell_size: 8,
-	});
+// Simulation variables
+var is_playing = true;
+var updates_per_second = 10;
 
-	setInterval(() => {
-		automata.update();
-	}, 1000 / 10);
-}, 0);
+// Create a cellular automata with window size
+const automata = new CellularAutomata({
+	world,
+	width: Math.floor(window.innerWidth / 8),
+	height: Math.floor(window.innerHeight / 8),
+	cell_size: 8,
+});
+
+// Update the automata
+function update_automata() {
+	const update_time = 1000 / updates_per_second;
+	const start_time = performance.now();
+	if (is_playing) automata.update();
+	const end_time = performance.now();
+	const elapsed_time = end_time - start_time;
+
+	setTimeout(update_automata, update_time - elapsed_time);
+}
+
+update_automata();
+
+// Pause/resume the simulation when the spacebar is pressed
+document.addEventListener("keydown", (e) => {
+	if (e.code === "Space") is_playing = !is_playing;
+});
