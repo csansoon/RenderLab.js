2f2674f23bd3d794d130f11eef2deaf4319bc63f
Carlos Sansón <57395395+csansoon@users.noreply.github.com>
2023-06-15 22:11:17 +0200

    Adjusted Cellular automata to viewport's size

--- examples/cellular-automata/index.js
+++ examples/cellular-automata/index.js
@@ -6,13 +6,16 @@ const world = new World();
 const viewport = world.createViewport({ element: "#canvas" });
 viewport.start(60);
 
-const automata = new CellularAutomata({
-	world,
-	width: 200,
-	height: 200,
-	cell_size: 50,
-});
+// when document is ready
+setTimeout(() => {
+	const automata = new CellularAutomata({
+		world,
+		width: Math.floor(viewport.size.x / 8),
+		height: Math.floor(viewport.size.y / 8),
+		cell_size: 8,
+	});
 
-setInterval(() => {
-	automata.update();
-}, 1000 / 10);
+	setInterval(() => {
+		automata.update();
+	}, 1000 / 10);
+}, 0);
